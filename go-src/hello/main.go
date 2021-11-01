49a17ab134a77bd763f49ca20491a4d48cb9adbb
Esmaeil mazahery <smaeil.mazahery@gmail.com>
2021-11-02 02:35:29 +0330

    up

--- go-src/hello/main.go
+++ go-src/hello/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	name := request.QueryStringParameters["name"]
-	var mongo = os.Getenv("mongo")
-	response := fmt.Sprintf("mongo : %s %s!", mongo, name)
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// name := request.QueryStringParameters["name"]
+
+	col := getClient().Database("test").Collection("test-col")
+
+	insertResult, err := col.InsertOne(ctx, bson.M{})
+	if err != nil {
+		return nil, errors.New("error")
+	}
+
+	response := fmt.Sprintf("id : %s!", insertResult.InsertedID.(primitive.ObjectID).Hex())
 
 	return &events.APIGatewayProxyResponse{
 		StatusCode:        200,
@@ -20,6 +36,25 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 		Headers:           map[string]string{"Content-Type": "text/html; charset=UTF-8"},
 		Body:              response,
 	}, nil
+}
+
+func getClient() *mongo.Client {
+	var dburi = os.Getenv("mongo")
+	clientOptions := options.Client().ApplyURI(dburi)
+
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return client
 }
 
 func main() {
