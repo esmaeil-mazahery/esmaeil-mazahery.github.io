5970038c1043799dc62d2fe53afc564a9f26a5ea
Esmaeil mazahery <smaeil.mazahery@gmail.com>
2021-11-01 21:51:40 +0330

    up

---
+++ go-src/hello/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	name := request.QueryStringParameters["name"]
+	response := fmt.Sprintf("Hello %s!", name)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Headers:           map[string]string{"Content-Type": "text/html; charset=UTF-8"},
+		Body:              response,
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
